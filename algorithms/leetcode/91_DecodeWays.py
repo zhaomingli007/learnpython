af6d9cea93f1c6a9e9c76a142b5dd738e8a63f15
Mingli Zhao <dev@ainew.io>
2021-12-29 17:38:15 +0800

    ADD more DP problems

--- algorithms/leetcode/91_DecodeWays.py
+++ algorithms/leetcode/91_DecodeWays.py
@@ -1,24 +1,14 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
+        if not s: return 1
         if s[0] == '0': return 0
-        def dp(i):
-            """return 
-            number of ways decoding till i-1 and i-2"""
-
-            if i == 0 and s[i] == '0':
-                return 0, 0
-            if i == 0:
-                return 0, 1
-            pre2, pre = dp(i-1)
-            if s[i] == '0':
-                return pre, pre
-            elif int(s[i-1:i]) <= 26:
-                return pre, max(1 + pre, 2+pre2)
-            else: return pre, pre+1
-        return max(dp(i)[1] for i in range(len(s)))
+        ans = self.numDecodings(s[1:])
+        if len(s)>1 and (s[0] == '1' or (s[0] =='2' and s[1]<='6')):
+            ans += self.numDecodings(s[2:])
+        return ans
 
 if __name__ == '__main__':
     s = Solution()
-    r = s.numDecodings('12')
+    r = s.numDecodings('10238712')
     print(r)
     
