f226ae4a79f74f3e884fe8566c88426604c483d3
Mingli Zhao <mingli@sunray.com.sg>
2022-01-29 18:28:07 +0800

    ADD letter case permutation

---
+++ algorithms/leetcode/36_ValidSudoku.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        """
+        Each row must contain the digits 1-9 without repetition.
+        Each column must contain the digits 1-9 without repetition.
+        Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.        
+        [["5","3",".",".","7",".",".",".","."]
+        ,["6",".",".","1","9","5",".",".","."]
+        ,[".","9","8",".",".",".",".","6","."]
+        ,["8",".",".",".","6",".",".",".","3"]
+        ,["4",".",".","8",".","3",".",".","1"]
+        ,["7",".",".",".","2",".",".",".","6"]
+        ,[".","6",".",".",".",".","2","8","."]
+        ,[".",".",".","4","1","9",".",".","5"]
+        ,[".",".",".",".","8",".",".","7","9"]]
+        Output: true        
+        """
--- algorithms/leetcode/784_LetterCasePermutation.py
+++ algorithms/leetcode/784_LetterCasePermutation.py
@@ -6,12 +6,29 @@ class Solution:
         """
         Input: s = "a1b2"
         Output: ["a1b2","a1B2","A1b2","A1B2"]
+        'A' -ASCII-> 65
+        'a' --> 97
+        '0' --> 48
+        diff: 32
+        
         """
         n = len(s)
         l = []
-        def bt(sub, p):
+        def bt(i, p):
             if len(p) == n:
-                l.append(p)
-            for i in range(len(sub)):
-                if i 
-            
+                l.append(''.join(p))
+                return
+            for j in range(i, n):
+                p.append(s[j])
+                bt(j+1, p)
+                p.pop()
+                if s[j].isalpha():
+                    p.append(s[j].upper() if ord(s[j])>=97 else s[j].lower())
+                    bt(j+1, p)
+                    p.pop()
+        bt(0, [])
+        return l
+if __name__ == '__main__':
+    s = Solution()
+    print(s.letterCasePermutation('a1b2C'))
+    
