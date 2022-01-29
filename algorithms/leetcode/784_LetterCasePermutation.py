a4a74d777c668af7152b111900ea8086b1ce990d
Mingli Zhao <mingli@sunray.com.sg>
2022-01-29 11:07:40 +0800

    ADD add combination algorithms

--- algorithms/leetcode/784_LetterCasePermutation.py
+++ algorithms/leetcode/784_LetterCasePermutation.py
@@ -7,3 +7,11 @@ class Solution:
         Input: s = "a1b2"
         Output: ["a1b2","a1B2","A1b2","A1B2"]
         """
+        n = len(s)
+        l = []
+        def bt(sub, p):
+            if len(p) == n:
+                l.append(p)
+            for i in range(len(sub)):
+                if i 
+            
